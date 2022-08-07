c6c4ed92c915aa43d5d7fe897a9ec9ea8de539ff
KarpovaO <grose@yandex.ru>
2022-08-07 19:54:04 +0300

    Create gradle.yml

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,30 +1,38 @@
 package ru.netology.service;
 
+import org.junit.Test;
 
-@org.testng.annotations.Test
+
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
-    void shouldCalculateCashbackHackServiceTestMoreThenMax() {
+    @Test
+    public void shouldCalculateCashbackHackServiceTestMoreThenMax() {
         CashbackHackService service = new CashbackHackService();
         int remain = service.remain(1001);
-        org.testng.Assert.assertEquals(remain, 999);
+        org.junit.Assert.assertEquals(999, remain);
 
     }
 
-    @org.testng.annotations.Test
-    void shouldCalculateCashbackHackServiceTestLessThenMax() {
+    @Test
+    public void shouldCalculateCashbackHackServiceTestLessThenMax() {
         CashbackHackService service = new CashbackHackService();
-        int remain = service.remain(0);
-        org.testng.Assert.assertEquals(remain, 1000);
+        int remain = service.remain(0); 
+        org.junit.Assert.assertEquals(1000, remain);
 
 
     }
 
-    @org.testng.annotations.Test
-    void shouldCalculateCashbackHackServiceTestNegative() {
+    @Test
+    public void shouldCalculateCashbackHackServiceTestNegative() {
         CashbackHackService service = new CashbackHackService();
         int remain = service.remain(-1);
-        org.testng.Assert.assertEquals(remain, 1001);
+        org.junit.Assert.assertEquals(1001, remain);
+    }
+
+    @Test
+    public void shouldCalculateCashbackHackServiceTest1000() {
+        CashbackHackService service = new CashbackHackService();
+        int remain = service.remain(1000);
+        org.junit.Assert.assertEquals(0, remain);
 
     }
 }
